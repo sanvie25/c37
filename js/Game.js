1171fd828115d503a16e3c2708378556d2a7d5b7
sanvie25 <56310223+sanvie25@users.noreply.github.com>
2020-03-21 18:53:50 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,55 @@
+class Game{
+    constructor(){}
+    getState(){
+     var gameStateref = database.ref('gameState');
+     gameStateref.on("value",function(data){
+         gameState = data.val();
+     })        
+    }
+    update(state){
+        database.ref('/').update({
+            gameState:state
+        })
+
+    }
+    async start(){
+      if(gameState===0){
+          player = new Player();
+          var playercountref = await database.ref('playerCount').once("value");
+          if(playercountref.exists()){
+              playerCount = playercountref.val();
+              
+          
+          player.getCount();
+          }
+          form = new Form();
+          form.display();      
+    }
+    }
+
+    play(){
+        form.hide();
+        text("game start",120,100);
+        Player.getplayerinfo();
+        if (allPlayers!==undefined){
+            var displayposition = 130;
+            for(var plr in allPlayers){
+            if(plr === "player"+player.index)
+            fill("red");
+            else
+            fill("black");
+
+            
+            displayposition+=30;
+            text(allPlayers[plr].name+":"+allPlayers[plr].distance,120,displayposition);
+        }
+        
+        }
+        if(keyIsDown(UP_ARROW)&&player.index!==null){
+            player.distance+=50;
+            player.update();
+        }
+    }
+}  
+   
+      
